bf434e4b15778c6fa89452a7fa6b2b781c1358b1
helvin159 <helvin159@gmail.com>
2024-02-28 13:33:57 -0500

    small updsates

--- src/components/BookingWidget/BookingWidget.js
+++ src/components/BookingWidget/BookingWidget.js
@@ -5,23 +5,44 @@ const BookingWidget = () => {
 		<div className='container w-3/4 bg-light-gray rounded-3xl translate-y-n50 py-8 px-0 mx-auto my-0 hidden tablet:block'>
 			<div className='container flex flex-row justify-evenly items-center mx-auto'>
 				<div>
-					<h4>Service</h4>
+					<label htmlFor='treatmentName' className='block px-2'>
+						Service
+					</label>
 					<input
-						className='bg-light-gray'
+						id='treatmentName'
+						className='bg-light-gray rounded-lg placeholder:text-black focus:outline-1 active:outline-0 focus:outline-variant-one py-2 px-2'
 						placeholder='What treatment are you looking for?'
 					/>
 				</div>
 				<div>
-					<h4>Date &amp; Time</h4>
-					<input className='bg-light-gray' placeholder='Select a date' />
+					<label htmlFor='date' className='block px-2'>
+						Date &amp; Time
+					</label>
+					<input
+						id='date'
+						className='bg-light-gray rounded-lg placeholder:text-black focus:outline-1 active:outline-0 focus:outline-variant-one py-2 px-2'
+						placeholder='Select a date'
+					/>
 				</div>
 				<div>
-					<h4>Duration</h4>
-					<input className='bg-light-gray' placeholder='Select duration' />
+					<label htmlFor='duration' className='block px-2'>
+						Duration
+					</label>
+					<input
+						id='duration'
+						className='bg-light-gray rounded-lg placeholder:text-black focus:outline-1 active:outline-0 focus:outline-variant-one py-2 px-2'
+						placeholder='Select duration'
+					/>
 				</div>
 				<div>
-					<h4>Technician</h4>
-					<input className='bg-light-gray' placeholder='Select a technician' />
+					<label htmlFor='technician' className='block px-2'>
+						Technician
+					</label>
+					<input
+						id='technician'
+						className='bg-light-gray rounded-lg placeholder:text-black focus:outline-1 active:outline-0 focus:outline-variant-one py-2 px-2'
+						placeholder='Select a technician'
+					/>
 				</div>
 				<div className='relative'>
 					<button className='bg-variant-one text-white rounded-lg py-3 px-5'>
